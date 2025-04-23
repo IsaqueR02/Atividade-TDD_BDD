9a3908fa289ae9bf199165f354b965e16e424766
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2025-04-23 20:41:36 -0300

    Adicionando arquivos da atividade

---
+++ atividade_BDD-HamburgueriaCucumber/sistema-de-hamburgueria/src/main/java/com/mycompany/sistema/de/hamburgueria/SistemaDeHamburgueria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema.de.hamburgueria;
+
+/**
+ *
+ * @author aluno
+ */
+public class SistemaDeHamburgueria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
